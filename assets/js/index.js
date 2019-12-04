84f91bc0c975730fcd92e326e42cc77f3cac62cc
xaralis <filip.varecha@fragaria.cz>
2019-12-04 15:44:56 +0100

    Improve visuals

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,14 @@
 // Import CSS bundle.
 require('../sass/main.scss');
 
+// Smooth scrolling
+import smoothscroll from 'smoothscroll-polyfill';
+smoothscroll.polyfill();
+
 // BulmaJS - initialize JS handlers for the Bulma CSS framework.
 // Only the plugins you need
 import Navbar from '@vizuaalog/bulmajs/src/plugins/navbar';
 import Notification from '@vizuaalog/bulmajs/src/plugins/notification';
-import Tabs from '@vizuaalog/bulmajs/src/plugins/tabs';
+// import Tabs from '@vizuaalog/bulmajs/src/plugins/tabs';
+
+
