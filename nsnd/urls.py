50fac5cce29303b2b638ddff16648fe0c8e912e2
xaralis <filip.varecha@fragaria.cz>
2019-12-05 13:09:55 +0100

    More visuals

--- nsnd/urls.py
+++ nsnd/urls.py
@@ -24,7 +24,8 @@ from django.views.generic import TemplateView
 sitemaps = {}
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="core/home.html")),
+    path("", TemplateView.as_view(template_name="core/home.html"), name="home"),
+    path("pripojit-podpis/", TemplateView.as_view(template_name="core/sign.html"), name="sign"),
     path("admin/", admin.site.urls),
     path(
         "sitemap.xml",
