5ca0dd46ded285c40821714613bf668c9834a5cf
xaralis <filip.varecha@fragaria.cz>
2019-12-05 13:29:50 +0100

    Show percents

--- nsnd/context_processors.py
+++ nsnd/context_processors.py
@@ -16,10 +16,12 @@ def votes(request):
         return {
             "votes_collected": profile.votes_collected,
             "votes_targeted": profile.votes_targeted,
+            "votes_percent": (profile.votes_targeted / profile.votes_collected) * 100
         }
     except Exception as exc:
         return {
             "votes_collected": 0,
             "votes_targeted": 0,
+            "votes_percent": 0,
         }
 
